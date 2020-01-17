a9ea04cf346500bd2ac9105306fe91d2f1d75b01
Aries Cs <aries0d0f@gmail.com>
2020-01-17 18:34:58 +0800

    [Layout] Add useful methods.

--- src/SessionTable.vue
+++ src/SessionTable.vue
@@ -4,16 +4,12 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import _ from 'lodash';
 
-import { IRoom, ISessionData } from './types/session';
+import { IRoom, ISessionData, ISession } from './types/session';
 
 @Component
 export default class CCIPSessionTable extends Vue {
-  @Prop({
-    required: true
-  })
-  private roomList!: IRoom['id'][];
-
   @Prop({
     default: false,
     required: false
@@ -24,6 +20,60 @@ export default class CCIPSessionTable extends Vue {
     required: true
   })
   private sessionData!: ISessionData;
+
+  private sessions!: ISession[];
+  private rooms!: IRoom['id'][];
+  private timeline!: string[];
+  private gridColString!: string;
+  private gridRowString!: string;
+
+  public created() {
+    this.preProcessSessionData();
+  }
+
+  private preProcessSessionData() {
+    this.processRoomData();
+    this.processTimelineData();
+    this.measureTableGrid();
+  }
+
+  private processRoomData() {
+    this.rooms = _.map(this.sessionData.rooms, 'id');
+  }
+
+  private processTimelineData() {
+    const sessionStartTimeCollection: Date[] = _.map(
+      this.sessionData.sessions,
+      'start'
+    );
+    const sessionEndTimeCollection: Date[] = _.map(
+      this.sessionData.sessions,
+      'end'
+    );
+
+    this.timeline = _.map(
+      _.sortedUniq(
+        _.concat(sessionStartTimeCollection, sessionEndTimeCollection)
+      ),
+      this.timeParser
+    );
+  }
+
+  private measureTableGrid() {
+    this.gridColString = `[TIME] 60px ${_.join(this.rooms, ' 1fr ')} 1fr [END]`;
+
+    this.gridRowString = `[HEAD] auto ${_.join(
+      this.timeline,
+      ' auto '
+    )} auto [END]`;
+  }
+
+  private timeParser(datetime: Date | string): string {
+    return new Date(datetime).toLocaleTimeString('en-US', {
+      timeZone: 'Asia/Taipei',
+      hour12: false
+    });
+  }
 }
 </script>
 
