5d66067800052f69d73d80d1cfe698c683e19fde
Aries Cs <aries0d0f@gmail.com>
2020-01-17 22:29:08 +0800

    [Layout] Add & modify methods and add basic UI.

--- src/SessionTable.vue
+++ src/SessionTable.vue
@@ -5,14 +5,57 @@
       'grid-template-columns': gridColString,
       'grid-template-rows': gridRowString
     }"
-  ></div>
+  >
+    <div
+      v-for="time in timeline"
+      :key="`time-${time}`"
+      :style="{
+        'grid-column-start': 'TIME',
+        'grid-row-start': `T${time}`
+      }"
+      class="ccip-app ccip-session-block timeblock"
+    >
+      <p>{{ `${time.slice(0, 2)}:${time.slice(2, 4)}` }}</p>
+    </div>
+    <div
+      v-for="session in sessions"
+      :key="`session-${session.id}`"
+      :style="{
+        'grid-column-start': processSessionRoom(session).start,
+        'grid-column-end': processSessionRoom(session).end,
+        'grid-row-start': `T${timeParser(session.start)}`,
+        'grid-row-end': `T${timeParser(session.end)}`
+      }"
+      class="ccip-app ccip-session-block session-block"
+    >
+      <p class="ccip-app ccip-session-tags">
+        <span
+          v-for="tag in session.tags"
+          :key="`tag-${tag}`"
+          class="ccip-app ccip-session-tag"
+        >
+          {{ getTag(tag).zh.name }}
+        </span>
+      </p>
+      <h2 class="ccip-app ccip-session-title">{{ session.zh.title }}</h2>
+      <p class="ccip-app ccip-session-speakers">
+        <span
+          v-for="speaker in session.speakers"
+          :key="`speaker-${speaker}`"
+          class="ccip-app ccip-session-speaker"
+        >
+          {{ getSpeaker(speaker).zh.name }}
+        </span>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import _ from 'lodash';
 
-import { IRoom, ISessionData, ISession } from './types/session';
+import { IRoom, ISessionData, ISession, ITag, ISpeaker } from './types/session';
 
 @Component
 export default class CCIPSessionTable extends Vue {
@@ -27,8 +70,12 @@ export default class CCIPSessionTable extends Vue {
   })
   private sessionData!: ISessionData;
 
+  @Prop({
+    required: false
+  })
+  private rooms!: IRoom['id'][];
+
   private sessions: ISession[] = this.sessionData.sessions;
-  private rooms: IRoom['id'][] = [];
   private timeline: string[] = [];
   private gridColString: string = '';
   private gridRowString: string = '';
@@ -44,7 +91,13 @@ export default class CCIPSessionTable extends Vue {
   }
 
   private processRoomData() {
-    this.rooms = _.map(this.sessionData.rooms, 'id');
+    if (
+      !this.rooms ||
+      typeof this.rooms === 'undefined' ||
+      this.rooms.length === 0
+    ) {
+      this.rooms = _.map(this.sessionData.rooms, 'id');
+    }
   }
 
   private processTimelineData() {
@@ -63,10 +116,31 @@ export default class CCIPSessionTable extends Vue {
           _.concat(sessionStartTimeCollection, sessionEndTimeCollection),
           this.timeParser
         )
-      ),
-      this.timelineParser,
-      'desc'
+      )
     );
+  }
+
+  private processSessionRoom(
+    session: ISession
+  ): { start: IRoom['id']; end?: IRoom['id'] } {
+    if (session.broadcast && typeof session.broadcast !== 'undefined') {
+      return {
+        start: (_.first(this.rooms) ===
+        _.first(_.map(this.sessionData.rooms, 'id'))
+          ? _.first(session.broadcast)
+          : _.includes(session.broadcast, _.first(this.rooms))
+          ? _.first(this.rooms)
+          : _.first(session.broadcast)) as string,
+        end:
+          _.last(session.broadcast) === _.last(this.rooms)
+            ? 'END'
+            : this.rooms[_.indexOf(this.rooms, _.last(session.broadcast)) + 1]
+      };
+    } else {
+      return {
+        start: session.room
+      };
+    }
   }
 
   private measureTableGrid() {
@@ -90,8 +164,12 @@ export default class CCIPSessionTable extends Vue {
       .replace(/:/g, '');
   }
 
-  private timelineParser(datetime: Date | string): number {
-    return parseInt(this.timeParser(datetime));
+  private getSpeaker(id: ISpeaker['id']): ISpeaker {
+    return _.find(this.sessionData.speakers, ['id', id]) as ISpeaker;
+  }
+
+  private getTag(id: ITag['id']): ITag {
+    return _.find(this.sessionData.tags, ['id', id]) as ITag;
   }
 }
 </script>
